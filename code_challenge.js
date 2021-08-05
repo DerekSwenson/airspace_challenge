b4c9fa5c95ade6bf6e10fcc87d69e9e9875998eb
DerekSwenson <88503415+DerekSwenson@users.noreply.github.com>
2021-08-05 13:45:18 -0400

    Add files via upload

--- code_challenge.js
+++ code_challenge.js
@@ -25,7 +25,7 @@ describe('This will test the login component of the Airspace Code Challenge webs
 
         cy.get('input[name="username"]').type('BadUsername')
         cy.get('input[name="password"]').type('BadPassword')
-        cy.get('.radius').click()
+        cy.get('.radius').click() 
 
         cy.get('#flash.flash.error').should('be.visible').should('have.class', 'flash error').should('contain.text', 'Your username is invalid!')
 
@@ -103,11 +103,25 @@ describe('This will test the login component of the Airspace Code Challenge webs
         cy.location('pathname').should('eq','/login')
     }) 
 
-    it('Test that Elemental Selenium link will open in a new tab by making an assertion', () => {
+    it('Test that the Elemental Selenium link will open in a new tab by making an assertion', () => {
 
         cy.get('a[href="http://elementalselenium.com/"]').should('have.attr', 'target', '_blank')
         
     })
 
+    it('Test the GitHub img link works', () => {
 
-})
+        cy.get('a[href="https://github.com/tourdedave/the-internet"]').should('have.prop', 'href', 'https://github.com/tourdedave/the-internet')
+        //verify the correct href exists
+        
+        cy.get('a[href="https://github.com/tourdedave/the-internet"]').then(function ($a) {
+        const href = $a.prop('href')
+
+        //request the contents of the url, and drill into the the response body to verify that there is at least some valid content
+        cy.request(href).its('body').should('include', '</html>')
+        
+        })
+
+    }) 
+
+}) 
